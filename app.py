897e26052ff67ee1975a590c4929fc548872f92d
Ben Cunningham <bc7657@desales.edu>
2022-04-13 05:15:50 +0000

    update to thread refresh task

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask import jsonify
 import refresh_stats
+from threading import Thread
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,6 +10,7 @@ def home_page():
     return 'dbah stats api home page'
 
 @app.route('/queue_stats_refresh')
-async def queue_stats_refresh():
-    await refresh_stats.refresh_stats()
-    return "stats refresh queued"
+def queue_stats_refresh():
+    thread = Thread(target=refresh_stats.refresh_stats)
+    thread.start()
+    return jsonify({"task":"queue_stats_refresh","status":"success"})
